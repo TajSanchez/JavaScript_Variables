c86cd409edd502b9d1e2b65c7fb836b310e1b575
Taj Sanchez <tajmarie92@gmail.com>
2021-02-22 20:38:37 -0600

    Finished Assignment

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Taj";
+let lastName = "Sanchez";
+var age = 28;
 // Exercise 2
-
+let fullName = `${firstName} ${lastName}`
+console.log(fullName);
 // Exercise 3
+var city = "Hayden";
+var passtime = "Watching Anime"
+let myStory = `Hello, my name is ${fullName}. I live in ${city}. I love ${passtime}, and coding.`
+console.log(myStory)
